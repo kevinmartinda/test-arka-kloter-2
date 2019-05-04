e75940208ba4d2f624f754463d38361061dd73a1
Kevin Martinda Sodikin <43369306+kevinmartinda@users.noreply.github.com>
2019-05-04 20:24:59 +0700

    moga masih sempet

--- SoalNo1.js
+++ SoalNo1.js
@@ -1,5 +1,6 @@
 const x = (name, address, hobbies, is_married, school, skills) => {
-	return {name, address, hobbies, is_married, school, skills}
+	const obj = {name, address, hobbies, is_married, school, skills}
+	return JSON.stringify(obj)
 }
 
 console.log(x("marvin", "Purbalingga", ['watch anime', 'some hobbies2'], false, {key: "highSchool"}, [{PHP:"Sedang"}, {JS:"sedang"}, {MYSQL:"Lumayan"}]))
