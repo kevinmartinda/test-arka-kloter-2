55bc59c35b783ee96a9c42e02ef99f9de15932c6
kevinmartinda <kevin36.k3@gmail.com>
2019-05-04 10:40:14 +0700

    Perubahan dikit

--- SoalNo1.js
+++ SoalNo1.js
@@ -2,4 +2,4 @@ const x = (name, address, hobbies, is_married, school, skills) => {
 	return {name, address, hobbies, is_married, school, skills}
 }
 
-console.log(x("marvin", "Purbalingga", ['watch anime', 'some hobbies2'], false, {key: "highSchool"}, ["PHP, JS, MYSQL"]))
+console.log(x("marvin", "Purbalingga", ['watch anime', 'some hobbies2'], false, {key: "highSchool"}, [{PHP:"Sedang"}, {JS:"sedang"}, {MYSQL:"Lumayan"}]))
